7d1541ca728e9d6815871de550f3d6c992a1ef6a
BradleySeymourSAE <1023303@student.sae.edu.au>
2021-04-01 03:25:12 +1000

    Fixed up scene structure ready to implement some UI - Start Menu and hooking that all up in reference to the lunar lander project created earlier. TODO: Add Backing track (Rezz - Delusion) as background audio for main menu

--- Assets/Scripts/Game/FieldOfFireEvents.cs
+++ Assets/Scripts/Game/FieldOfFireEvents.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameEventsManager : MonoBehaviour
+/// <summary>
+///		Static Class for Handling the Field Of Fire Game Events 
+/// </summary>
+public static class FieldOfFireEvents
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#region Event Delegates
+	// Game Mode Events 
+	public delegate void RestartGame(); // restart the game to first round  
+	public delegate void ResetWave(); // Resets the current wave 
+
+	public delegate void PreGame();
+	public delegate void GameStarted();
+	public delegate void PostGame(Player PlayerReference);
+
+	public delegate void UpdatePlayerKillCount(Player PlayerScoreToUpdate, int Amount);
+
+	// Player Spawn Events 
+	public delegate void SpawnPlayer(Transform PlayerTank);
+	public delegate void OnPlayerHasSpawned();
+
+	// Tanks 
+	public delegate void OnTankDestroyed(Transform Tank);
+	public delegate void ReceiveDamage(Transform ObjectReceivedDamage, float Amount);
+
+	// Tank Spawn Events 
+	public delegate void SpawnEnemyWave(int NumberOfInfantry, int NumberOfEnemyTanks);
+	public delegate void OnEnemyWaveSpawned(List<GameObject> EnemyAICharacters, List<GameObject> EnemyAITanks);
+
+	#endregion
+
+
+	// TODO: Need to complete these events
+	#region Game Mode Events  
+
+
+
+
+	#endregion
+
+	#region Player Events | UI Events  
+
+
+
+	#endregion
+
+	#region Enemy AI events 
+
+	#endregion
 }
